e2708461c962def143a5b0357e3c40442c5d0db5
hanselcolmenarez <hanselcolmenarez@hotmail.com>
2022-06-26 22:34:28 -0400

    Primer Commit

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+require('dotenv').config({ path: 'var.env' });
+
+const connectDB = async () => {
+
+    try {
+        
+        await mongoose.connect(process.env.MONGO_DB, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        console.log("Base de datos conectada");
+
+    } catch (error) {        
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const conectarBD = require('./config/db');
+const cors = require('cors');
+
+const app = express(); // creamos el servidor
+
+conectarBD(); // estableciendo conexión con la base de datos
+
+app.use( express.static('public') ); //directorios publicos
+
+app.use(express.json());
+app.use(cors());
+app.use('/api/comentarios', require('./routes/comentario'));
+
+app.listen(process.env.PORT, () => {
+    console.log("El servidor se ha iniciado en el puerto: " + process.env.PORT);
+})
